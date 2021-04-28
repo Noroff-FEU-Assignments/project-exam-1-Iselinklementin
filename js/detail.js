47af0dfa5e4e2a19f3de49774de5baab25e7e568
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-04-28 14:05:28 +0200

    added html detail

--- js/detail.js
+++ js/detail.js
@@ -11,11 +11,13 @@ function getApiDetail () {
     Promise.all([urlPost, urlTag])
     .then(values => Promise.all(values.map(value => value.json())))
     .then(finalValue => {
-        let urlResponse = finalValue[0];
-        let tagResponse = finalValue[1];
+        let place = finalValue[0];
+        let tag = finalValue[1];
 
-        console.log(urlResponse)
-        console.log(tagResponse)
+        createIntro(place, tag);
+
+        // console.log(place)
+        console.log(tag)
     })
 }   catch (error) {
         console.error(error);
@@ -23,3 +25,59 @@ function getApiDetail () {
 };
 
 getApiDetail();
+
+const intro = document.querySelector(".intro-text");
+const heading = document.querySelector("h1");
+const subheading = document.querySelector("h2");
+const tags = document.querySelector(".tags");
+const headImg = document.querySelector(".header-img");
+const ingress = document.querySelector(".ingress");
+const para = document.querySelector(".ingress-text");
+let list = [];
+
+function createIntro(place, tag) {
+    tagsStyle(place, tag);
+
+    console.log(place.acf.subtext)
+
+    // INTRO 
+
+    heading.innerHTML += place.acf.sub_heading;
+    tags.innerHTML += list;
+    tags.insertAdjacentHTML("afterend", place.content.rendered);
+    headImg.src = place.acf.heading_img.sizes.large;
+
+    // INGRESS
+
+    subheading.innerHTML += place.acf.sub_heading_1;
+    para.innerText += place.acf.subtext;
+    // subheading.insertAdjacentElement("afterbegin", place.acf.subtext);
+
+
+};
+
+function tagsStyle(place, tag) {
+    place.tags.filter(t => {
+        tag.forEach(e => {
+            if (e.id === t && e.name === "Asia" || e.id === t && e.name === "Adventure" || e.id === t && e.name === "Landscapes") {
+                list.push(`<li class="tag tag:before tag:after purple-tag purple-tag:before purple-tag:after purple-tag:hover:after purple-tag:hover:before purple-tag:hover">${e.name}</li>`)
+            }
+            
+            if (e.id === t && e.name === "Culture" || e.id === t && e.name === "Animals" || e.id === t && e.name === "America") {
+                list.push(`<li class="tag tag:before tag:after sand-tag sand-tag:before sand-tag:after sand-tag:hover:after sand-tag:hover:before sand-tag:hover">${e.name}</li>`)
+            }
+
+            if (e.id === t && e.name === "Nature" || e.id === t && e.name === "Wildlife") {
+                list.push(`<li class="tag tag:before tag:after green-tag green-tag:before green-tag:after green-tag:hover:after green-tag:hover:before green-tag:hover">${e.name}</li>`)
+            }
+
+            if (e.id === t && e.name === "Europe" || e.id === t && e.name === "Beliefs" ) {
+                list.push(`<li class="tag tag:before tag:after blue-tag blue-tag:before blue-tag:after blue-tag:hover:after blue-tag:hover:before blue-tag:hover">${e.name}</li>`)
+            }
+
+            if (e.id === t && e.name === "Oceania" || e.id === t && e.name === "Traditions" || e.id === t && e.name === "Africa") {
+                list.push(`<li class="tag tag:before tag:after peach-tag peach-tag:before peach-tag:after peach-tag:hover:after peach-tag:hover:before peach-tag:hover">${e.name}</li>`)
+            }
+        })
+    });
+}
