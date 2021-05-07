b569466f910264ea5ac39366096ec4d6cb27c387
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-05-07 12:41:56 +0200

    Browser adjustments

--- js/components.js
+++ js/components.js
@@ -11,6 +11,7 @@ function handleScroll() {
         navUl.classList.add("ul-onscroll");
 
         let child = navUl.children[3];
+
         if (child.firstChild.classList.contains("details")) {
             nav.classList.add("detail-scrolled");
         }
@@ -23,3 +24,20 @@ function handleScroll() {
 }
 
 window.addEventListener("scroll", handleScroll);
+
+function setWidthDesktop() {
+    if (window.innerWidth > 699) { 
+        logo.src = "/images/icons/logofeet_full.svg";
+        logo.classList.add("desktop-logo");
+    } 
+}
+
+function setWidthMobile() {
+    if (window.innerWidth < 700) {
+        logo.src = "/images/icons/logofeet.svg";
+        logo.classList.remove("desktop-logo");
+    }
+}
+
+window.addEventListener("resize", setWidthDesktop)
+window.addEventListener("resize", setWidthMobile)
